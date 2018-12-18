b14dfc4c8ca7a3395190d88f98a7c17c77503977
Azat <deathstoke97>
2018-12-18 19:37:17 +0600

    webpack config

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = ({name, age, children, click, changed}) => {
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick = {click}>I am {name} and I am {age}</p>
             <p>{children}</p>
             <input type="text" onChange = {changed} value={name}/>
