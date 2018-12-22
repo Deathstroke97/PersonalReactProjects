2edb1b9199ea72706ac6276bd6a552e88d5e4315
Azat <deathstoke97>
2018-12-22 20:46:58 +0600

    start project

--- cmp-deep-dive-context-start/cmp-deep-dive--context-start/src/components/Login.js
+++ cmp-deep-dive-context-start/cmp-deep-dive--context-start/src/components/Login.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 
 import  AuthContext  from '../auth-context';
 
-
-
-class Login extends Component {
+ class Login extends Component {
     static contextType = AuthContext;
     
     render() {
-      console.log('this.context: ', this.context, 'contextType: ', this.contextType);
-        return (
+      return (
             <button onClick={this.context.toggleAuth}>
               {this.context.isAuth ? 'Logout' : 'Login'}
             </button>
