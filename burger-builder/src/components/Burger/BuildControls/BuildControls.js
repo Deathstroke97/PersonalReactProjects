fe426d833392f7e4e41aabf4feb81ee3c851df45
Your Name <Deathstroke97>
2018-12-24 01:17:01 +0600

    burgerBuilding1

---
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { lable: 'Salad', type: 'salad' },
+    { lable: 'Bacon', type: 'bacon' },
+    { lable: 'Cheese', type: 'cheese' },
+    { lable: 'Meat', type: 'meat' },
+]
+
+const buildControls = (props) => (
+    <div className = {classes.BuildControls}>
+        { controls.map(ctrl => (
+        <BuildControl 
+            key = {ctrl.lable} 
+            label = {ctrl.lable}
+            added = {() => props.ingredientAdded(ctrl.type)}
+            removed = {() => props.ingredientRemoved(ctrl.type)}
+            disabled= {props.disabled[ctrl.type]}
+            />
+        ))}
+    </div>
+)
+
+export default buildControls;
--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxillary';
-
+import classes from './Layout.css'
 
 const layout = ( props ) => (
     <Aux>
         <div>Toolbar, SideDraw, Backdrop</div>
-        <main>
+        <main className = {classes.Content}>
             {props.children}
         </main>
     </Aux>
