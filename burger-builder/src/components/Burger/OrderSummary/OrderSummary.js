0d5e56a8005586ff43bcb6d08c78d1611750fd31
Azat <deathstoke97>
2018-12-26 20:53:44 +0600

    http request

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxillary';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxillary/Auxillary';
 import Button from '../../UI/Button/Button';    
 
-const orderSummary = (props) => {
-    const ingredients = Object.keys(props.ingredients).map(ingKey => {
-        return (<li key = {ingKey}>
-                    <span style = {{textTransform: 'capitalize'}}>{ingKey}</span> : {props.ingredients[ingKey]}
-                </li>)
-    })
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients: </p>
-            <ul>
-                {ingredients}
-            </ul>
-            <p><strong>Total price: {props.price.toFixed(2)} </strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType = "Danger" clicked = {props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType = "Success" clicked = {props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
+class OrderSummary extends Component {
+    
+    componentWillUnmount() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+
+    render() {
+        
+        const ingredients = Object.keys(this.props.ingredients).map(ingKey => {
+            return (<li key = {ingKey}>
+                        <span style = {{textTransform: 'capitalize'}}>{ingKey}</span> : {this.props.ingredients[ingKey]}
+                    </li>)
+        })
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients: </p>
+                <ul>
+                    {ingredients}
+                </ul>
+                <p><strong>Total price: {this.props.price.toFixed(2)} </strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType = "Danger" clicked = {this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType = "Success" clicked = {this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>   
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className = {classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked = {props.drawerToggleClicked}/>
         <div className = {classes.Logo}>
             <Logo />
         </div>
@@ -13,6 +14,6 @@ const toolbar = (props) => (
             <NavigationItems />
         </nav>
     </header>
-)
+);
 
 export default toolbar;
