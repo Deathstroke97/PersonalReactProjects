7cf9709fffcca23ae77483535640acd8eb8d7872
Your Name <Deathstroke97>
2018-12-27 01:11:47 +0600

    working with requests

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -9,8 +9,6 @@ class FullPost extends Component {
         loadedId: null,
     }
 
-  
-
     componentDidUpdate() {
         if (this.props.id) {
             if (!this.state.loadedPost || this.state.loadedPost && this.state.loadedPost.id !== this.props.id) {
@@ -22,8 +20,16 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            })
+    }
+
     render () {
         let post = <p style = {{textAlign: 'center'}}>Please select a Post!</p>;
+        
         if (this.props.id) {
             <p style = {{textAlign: 'center'}}>Loading...</p>;
         }
@@ -31,9 +37,9 @@ class FullPost extends Component {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.content}</p>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick = {this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
     
