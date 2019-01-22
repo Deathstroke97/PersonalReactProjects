e8508fe9845d94febc2d7864db8d82b5d2a3ee57
Your Name <Deathstroke97>
2019-01-23 00:42:18 +0600

    redux on burger

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import Aux from '../../hoc/Auxillary/Auxillary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -9,6 +10,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import { Route } from 'react-router-dom';
 import Checkout from '../Checkout/Checkout';
+import * as actionTypes from '../../store/actions';
 
 
 const INGREDIENT_PRICES = {
@@ -33,9 +35,9 @@ class BurgerBuilder extends Component {
 
 
     componentDidMount() {
-        axios.get('https://react-my-burger-a3686.firebaseio.com/ingredients.json')
-            .then(res => this.setState({ingredients: res.data}))
-            .catch(error => { this.setState({ error: true }) })
+        // axios.get('https://react-my-burger-a3686.firebaseio.com/ingredients.json')
+        //     .then(res => this.setState({ingredients: res.data}))
+        //     .catch(error => { this.setState({ error: true }) })
     }
 
 
@@ -153,4 +155,17 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default withErrorHandler(BurgerBuilder, axios); 
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios)); 
